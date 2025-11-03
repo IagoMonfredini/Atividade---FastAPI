663cee494742e93fffb1c16f8b9759535e9b8756
Iago Monfredini <iagomonfredini46171@gmail.com>
2025-11-03 14:31:45 -0300

    Streamlit menu função listar produtos✔

---
+++ front-end/app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import requests
+
+API_URL = "http://127.0.0.1:8000"
+st.set_page_config(page_title="Gerenciador de produtos", page_icon="🛒")
+st.title("🛒Estoque de produtos ")
+
+menu = st.sidebar.radio("Navegação", ["Estoque", "Registrar produto", "Atualizar produto", "Deletar produto"])
+
+if menu == "Estoque":
+    st.subheader("Todos os produtos disponiveis")
+    response = requests.get(f"{API_URL}/listar_produtos")
+    if response.status_code == 200:
+        produtos = response.json().get("produtos", [])
+        if produtos:
+            st.dataframe(produtos)
+        else:
+            st.info("Não há nenhum produto registrado")
+    else:
+        st.error("Erro ao acessar a API")
+    
